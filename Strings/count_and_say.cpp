aa0985a000768d4952ae06962e1682cfe3236aa6
Anand Pandey <43315386+r0cket007@users.noreply.github.com>
2021-04-29 12:55:41 +0530

    Update count_and_say.cpp

--- Strings/count_and_say.cpp
+++ Strings/count_and_say.cpp
@@ -9,51 +9,37 @@
 #define r0cket007 in out 
 using namespace std ;
 const int MAX = 500; 
-//-----------------------------------------------------------------------------------
-string longestPalin (string s)
-{
-    int n = s.length( ) ;
-    int start_idx = 0 ;
-    int end_idx = 0 ;
-    int mx = 1 ; 
-    for( int i = 0 ; i < n ; i ++ )
-    {
-        //for odd palindromic string
-        int l = i - 1 ;
-        int r = i + 1 ;
-        int ct = 1 ;
-        while( l >= 0 && r < n && s[ l ] == s[ r ] )
-        {
-            l -- ;
-            r ++ ;
-            ct += 2 ;
-        }
-        if( ct > mx )
-        {
-            mx = ct ;
-            start_idx = l + 1 ;
-            end_idx = r - 1 ;
-        }
-        //for even palindromic strings
-        l = i ;
-        r = i + 1 ;
-        ct = 0 ;
-        while( r < n && l >= 0 && s[ l ] == s[ r ] )
-        {
-            ct += 2 ;
-            l -- ;
-            r ++ ;
-        }
-        if( ct > mx )
-        {
-            mx = ct ;
-            start_idx = l + 1 ;
-            end_idx = r - 1 ;
-        }
 
+//-----------------------------------------------------------------------------------
+
+int counter = 1 ;
+string solve(string txt, int n)
+{
+    if(counter == n)
+        return txt;
+    int i = 0 ;
+    string pat ;
+    while(i < txt.length())
+    {
+        char ch = txt[i] ;
+        int ct = 0;
+        while(ch==txt[i])
+        {
+            i++;
+            ct++;    
+        }      
+        pat += (char)(ct+'0') ;
+        pat += ch ;     
     }
-    return s.substr( start_idx , (end_idx - start_idx + 1) ) ;
+    counter ++ ;
+    return solve( pat , n );
 }
+
+string countAndSay(int n) 
+{
+    return solve("1",n);
+}
+
 //-------------------------------------------------------------------------------------
 signed main( )
 {
@@ -64,9 +50,8 @@ signed main( )
     // cin >> testcases ;
     while( testcases -- )
     {
-        string S; 
-        cin >> S;
-        cout << longestPalin(S) << endl;
+        int n = 12 ;
+        cout << countAndSay(n);
     }
     return 0 ;
 }
