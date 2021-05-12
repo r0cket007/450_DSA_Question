e6fa6e081b77693cbedf70c5d8dc521f694765fd
r0cket007 <anandthegreat8@gmail.com>
2021-05-12 11:38:21 +0530

    Added files

--- Search & Sort/Kth_element_of_two_sorted_arrays.cpp
+++ Search & Sort/Kth_element_of_two_sorted_arrays.cpp
@@ -9,11 +9,40 @@ using namespace std ;
 class Solution
 {
 public:
+    int getElement(int *arr1, int *arr2, int *end1, int *end2, int k)
+    {
+        if(*arr1 == *end1)
+        {
+            return arr2[k];
+        }
+        if(*arr2 == *end2)
+        {
+            return arr1[k];
+        }
+        int mid1 = (end1 - arr1) / 2;
+        int mid2 = (end2 - arr2) / 2;
+        if(mid1 + mid2 < k)
+        {
+            if(arr1[mid1] > arr2[mid2])
+            {
+                return getElement(arr1, arr2 + mid2 + 1, end1, end2, k - mid2 - 1);
+            }
+            else
+            {
+                return getElement(arr1 + mid1 + 1, arr2, end1, end2, k - mid1 - 1);
+            }
+        }
+        else
+        {
+            if (arr1[mid1] > arr2[mid2])
+                return getElement(arr1, arr2, arr1 + mid1, end2, k);
+            else
+                return getElement(arr1, arr2, end1, arr2 + mid2, k);
+        }
+    }
     int kthElement(int arr1[], int arr2[], int n, int m, int k)
     {
-        int arr3[n + m];
-        merge(arr1, arr1 + n, arr2, arr2 + m, arr3);
-        return arr3[k - 1];
+        return getElement(arr1, arr2, arr1 + n, arr2 + m, k - 1);
     }
 };
 //--------------------------------------------------------------------
