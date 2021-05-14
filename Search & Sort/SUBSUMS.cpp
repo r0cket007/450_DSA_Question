775b986e77820f23e706dc3aa3e595ce7beb3d38
r0cket007 <anandthegreat8@gmail.com>
2021-05-14 11:24:57 +0530

    Added files

--- Search & Sort/SUBSUMS.cpp
+++ Search & Sort/SUBSUMS.cpp
@@ -1,42 +1,37 @@
 /*
-    PRATA - ANARC05B
-    Link: https://www.spoj.com/problems/ANARC05B/
+    SUBSUMS - Subset Sums
+    Link: https://www.spoj.com/problems/SUBSUMS/
 */
 #include <bits/stdc++.h>
-#define int long long
+#define int long long 
 using namespace std;
-#define in freopen("input.txt", "r", stdin);
-#define out freopen("output.txt", "w", stdout);
-#define r0cket007 in out
 //--------------------------------------------------------------------
 class Solution
 {
 public:
-    long long ans = 0;
-    long long a, b;
-    vector<long long> getSubsets(vector<long long> &s, int start, int siz)
+    int ans = 0;
+    void getSubsets(vector<int> &s, vector<int> &temp, int start, int siz)
     {
-        vector<long long> temp;
         for (int j = 0; j < pow(2, siz); j++)
         {
-            long long sum = 0;
+            int sum = 0;
             for (int i = start; i <= start + siz; i++)
             {
-                if (j & (1 << (i - start)))
+                if (j&(1<<(i - start)))
                 {
                     sum += s[i];
                 }
             }
             temp.push_back(sum);
         }
-        return temp;
     }
-    int subsets(vector<long long> &s, int a, int b)
+    int subsets(vector<int> &s, int a, int b)
     {
         int n = s.size();
-        vector<long long>::iterator low, high;
-        vector<long long> left = getSubsets(s, 0, n / 2);
-        vector<long long> right = getSubsets(s, n / 2, n & 1 ? n / 2 + 1 : n / 2);
+        vector<int>::iterator low, high;
+        vector<int> left, right;
+        getSubsets(s, left, 0, n >> 1);
+        getSubsets(s, right, n >> 1, (n+1) >> 1);
         sort(right.begin(), right.end());
         for (int i = 0; i < left.size(); i++)
         {
@@ -50,9 +45,11 @@ public:
 //--------------------------------------------------------------------
 signed main()
 {
-    // r0cket007
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
     int testcases = 1;
-    cin >> testcases;
+    // cin >> testcases;
     while (testcases)
     {
         int n, a, b;
