7264b1543714edd8929d92468454a6adedff1c40
r0cket007 <anandthegreat8@gmail.com>
2021-05-17 20:01:09 +0530

    Added files

---
+++ LinkedList/Reverse a Doubly Linked List.cpp
@@ -0,0 +1,115 @@
+/*
+    Reverse a Doubly Linked List 
+    Link: https://practice.geeksforgeeks.org/problems/reverse-a-doubly-linked-list/1#
+*/
+#include <bits/stdc++.h>
+using namespace std;
+struct Node
+{
+    int data;
+    Node *next;
+    Node *prev;
+    Node(int x)
+    {
+        data = x;
+        next = NULL;
+        prev = NULL;
+    }
+};
+Node *newNode(int data)
+{
+    Node *temp = new Node(data);
+
+    return temp;
+}
+void displayList(Node *head)
+{
+    while (head->next)
+    {
+        cout << head->data << " ";
+        head = head->next;
+    }
+    cout << head->data;
+}
+
+int getLength(Node *head)
+{
+    Node *temp = head;
+
+    int count = 0;
+    while (temp->next != head)
+    {
+        count++;
+        temp = temp->next;
+    }
+    return count + 1;
+}
+
+bool verify(Node *head)
+{
+    int fl = 0;
+    int bl = 0;
+
+    Node *temp = head;
+
+    while (temp->next)
+    {
+        temp = temp->next;
+        fl++;
+    }
+
+    while (temp->prev)
+    {
+        temp = temp->prev;
+        bl++;
+    }
+
+    return fl == bl;
+}
+//--------------------------------------------------------------------------------------
+Node *reverseDLL(Node *head)
+{
+    Node *current = head, *dummy = NULL;
+    while (current != NULL)
+    {
+        Node *temp = current->next;
+        current->next = dummy;
+        current->prev = temp;
+        dummy = current;
+        current = temp;
+    }
+    return dummy;
+}
+//---------------------------------------------------------------------------------------
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        Node *head = NULL, *tail = NULL;
+        int x;
+        cin >> x;
+        head = newNode(x);
+        tail = head;
+
+        for (int i = 0; i < n - 1; i++)
+        {
+            cin >> x;
+            Node *temp = newNode(x);
+            tail->next = temp;
+            temp->prev = tail;
+            tail = temp;
+        }
+        head = reverseDLL(head);
+
+        if (verify(head))
+            displayList(head);
+        else
+            cout << "Your pointers are not correctly connected";
+        cout << endl;
+    }
+    return 0;
+}
