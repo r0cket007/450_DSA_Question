182f0b2006c5a6b6d09292fa7c139e4a3617ff96
r0cket007 <anandthegreat8@gmail.com>
2021-06-05 20:53:00 +0530

    Added files

---
+++ Binary Tree/Reverse Level Order Traversal.cpp
@@ -0,0 +1,103 @@
+/*
+    Reverse Level Order Traversal
+    Link: https://practice.geeksforgeeks.org/problems/reverse-level-order-traversal/1#
+*/
+#include <bits/stdc++.h>
+using namespace std;
+struct Node
+{
+    int data;
+    struct Node *left;
+    struct Node *right;
+};
+Node *newNode(int val)
+{
+    Node *temp = new Node;
+    temp->data = val;
+    temp->left = NULL;
+    temp->right = NULL;
+
+    return temp;
+}
+Node *buildTree(string str)
+{
+    if (str.length() == 0 || str[0] == 'N')
+        return NULL;
+    vector<string> ip;
+    istringstream iss(str);
+    for (string str; iss >> str;)
+        ip.push_back(str);
+    Node *root = newNode(stoi(ip[0]));
+    queue<Node *> queue;
+    queue.push(root);
+    int i = 1;
+    while (!queue.empty() && i < ip.size())
+    {
+        Node *currNode = queue.front();
+        queue.pop();
+        string currVal = ip[i];
+        if (currVal != "N")
+        {
+            currNode->left = newNode(stoi(currVal));
+            queue.push(currNode->left);
+        }
+        i++;
+        if (i >= ip.size())
+            break;
+        currVal = ip[i];
+        if (currVal != "N")
+        {
+            currNode->right = newNode(stoi(currVal));
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+
+    return root;
+}
+vector<int> reverseLevelOrder(Node *root);
+
+int main()
+{
+
+    int t;
+    scanf("%d ", &t);
+    while (t--)
+    {
+        string s;
+        getline(cin, s);
+        Node *root = buildTree(s);
+        vector<int> result = reverseLevelOrder(root);
+        for (int i = 0; i < result.size(); ++i)
+        {
+            cout << result[i] << " ";
+        }
+        cout << endl;
+    }
+    return 1;
+} 
+//-----------------------------------------------------------------------------------------------
+vector<int> reverseLevelOrder(Node *node)
+{
+    vector<int> ans;
+    queue<Node *> q;
+    q.push(node);
+    while (true)
+    {
+        int n = q.size();
+        if (n == 0)
+            break;
+        while (n--)
+        {
+            Node *temp = q.front();
+            q.pop();
+            if (temp->right != NULL)
+                q.push(temp->right);
+            if (temp->left != NULL)
+                q.push(temp->left);
+            ans.emplace(ans.begin(), temp->data);
+        }
+    }
+    return ans;
+}
+//-----------------------------------------------------------------------------------------------
